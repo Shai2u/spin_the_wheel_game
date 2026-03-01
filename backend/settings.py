a06517b2daf71fa0b6d5c3f91d0748f91cebca99
Shai Sussman <shai@simplex3d.com>
2026-03-01 09:28:46 +0200

    Update ALLOWED_HOSTS in settings.py to include localhost and testserver

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z-o1fnvefz$gb7#(n6o)$&-)x-w*&&(ld70^p2#)eee*6mbgat
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "testserver"]
 
 
 # Application definition
